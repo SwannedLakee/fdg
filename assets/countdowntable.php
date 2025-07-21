bdb97b379b5a2ac2734f75f2931afd127d451dfa
o yoga <o28o@mail.ru>
2025-07-21 14:07:54 -0400

    update from server

--- assets/countdowntable.php
+++ assets/countdowntable.php
@@ -209,7 +209,7 @@ $totalinyears years total
 <a target=_blank href=/r/?q=sn22.102>sn22.102</a>  
 <a target=_blank href=/r/?q=sn35.70>sn35.70</a>  
 <a target=_blank href=/r/?q=an3.101>an3.101</a>  
-<a target=_blank href=/assets/audio/documents/dn_Syrkin.pdf>ДН пер. Сыркин А.Я.</a>  
+<a target=_blank href=/games/sanskrit.com.ua/en/devanagari-game/index.html>Sanskrit Game>  
 <a target=_blank href=http://titus.uni-frankfurt.de/texte/etcs/ind/aind/bskt/lankavat/lanka.htm>TITUS</a>  
 <a target=_blank href=http://spiritual.su/>spiritual.su</a> 
 <a target=_blank href=http://probud.narod.ru/dop.html>Probud.narod.ru</a> 
