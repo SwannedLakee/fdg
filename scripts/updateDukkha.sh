5c8c390c13aa2b5b7d1f85ecb7a852a29d324fc9
Ubu kv <ubu@fdg.com>
2025-07-23 02:12:25 +0500

    updates

---
+++ scripts/updateDukkha.sh
@@ -0,0 +1,26 @@
+cd /var/www/html/assets/texts/sutta
+
+#asset
+grep -riE "\bстрадан" * | grep -v ru-o.json > /var/www/html/all.txt
+grep -riE "\bстрадан" * | grep -v ru-o.json | grep edited > /var/www/html/edited.txt
+
+
+echo >> /var/www/html/scripts/updateDukkha.sh
+date >> /var/www/html/scripts/updateDukkha.sh
+wc -l /var/www/html/all.txt /var/www/html/edited.txt  | tee -a /var/www/html/scripts/updateDukkha.sh
+cd - 
+
+
+exit 0
+
+
+
+
+  1090 /var/www/html/all.txt
+   265 /var/www/html/edited.txt
+  1355 total
+
+Wed Jul 23 01:58:52 AM +05 2025
+  1090 /var/www/html/all.txt
+   265 /var/www/html/edited.txt
+  1355 total
