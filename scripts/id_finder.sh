610a628d678ba4ff6e6417b2a7aaf4c7c90da1e3
o yoga <o28o@mail.ru>
2025-07-25 09:52:47 -0400

    update from server

--- scripts/id_finder.sh
+++ scripts/id_finder.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-  read -p "Enter string (e.g. an1.30:1.1): " input
-else
-  input="$1"
-fi
-
 paths=(
   "../suttacentral.net/sc-data/sc_bilara_data/root/pli/ms/"
   "assets/texts/sutta"
   "../suttacentral.net/sc-data/sc_bilara_data/translation/en/"
 )
 
-# Ищем и собираем результат в одну строку, убираем ошибки
-result=$(grep -m1 -r --color=always "$input" "${paths[@]}" 2>/dev/null | tr '\n' ' ')
+echo "Enter strings (e.g. an1.30:1.1), Ctrl+D to exit."
 
-echo "$result"
+while IFS= read -r input; do
+  if [[ -z "$input" ]]; then
+    continue
+  fi
 
-prefix="${input%%:*}"
-suffix="${input#*:}"
-echo "https://dhamma.gift/r/?q=${prefix}#${suffix}"
+  # Ищем и выводим результат grep + awk (без ошибок)
+  grep -m1 -r --color=always "$input" "${paths[@]}" 2>/dev/null | \
+    awk -F':' '{for(i=2; i<=NF; i++) printf "%s%s", $i, (i==NF ? "\n" : ":")}'
+
+  prefix="${input%%:*}"
+  suffix="${input#*:}"
+  echo "https://dhamma.gift/r/?q=${prefix}#${suffix}"
+echo 
+echo 
+echo
+done
