8e10dfb70a22bb07db86381a483bae1514cd147f
o yoga <o28o@mail.ru>
2025-07-30 16:27:43 -0400

    update from server

--- read/js/devanagari.js
+++ read/js/devanagari.js
@@ -90,7 +90,7 @@ else {
 var rootDevanagaripath = `/assets/texts/devanagari/root/pli/ms/${texttype}/${slugReady}_rootd-pli-ms.json`;
  }
 
-var rustrnpath = `/assets/texts/${texttype}/${slugReady}_translation-${pathLang}-${translator}.json`;
+var rustrnpath = `/assets/texts/ru/${texttype}/${slugReady}_translation-${pathLang}-${translator}.json`;
 
 if ( texttype === "vinaya")
 {
@@ -169,7 +169,7 @@ else {
 var rootDevanagaripath = `/assets/texts/devanagari/root/pli/ms/${texttype}/${slug}_rootd-pli-ms.json`;
  }
   
-    var trnpath = `/assets/texts/${texttype}/${slug}_translation-${pathLang}-${translator}.json`;
+    var trnpath = `/assets/texts/ru/${texttype}/${slug}_translation-${pathLang}-${translator}.json`;
     var engtrnpath = `${Sccopy}/sc-data/sc_bilara_data/translation/en/brahmali/${texttype}/${slug}_translation-en-brahmali.json`;
     var htmlpath = `/assets/html/${texttype}/${slug}_html.json`;
 
