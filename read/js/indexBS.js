ff0ba0728a218cefe82183132f840049c2040352
Your Name <you@example.com>
2026-01-16 15:16:19 +0700

    updates

--- read/js/indexBS.js
+++ read/js/indexBS.js
@@ -374,66 +374,6 @@ if (textUrl) {
 scLink += `<a target="" title="Digital Pali Reader" href="${textUrl}">DPR</a>&nbsp;`;
 }
 }
-//dpr end//dpr
-if (texttype !== "vinaya") {
-function getTextUrl(slug) {
-  let nikaya = slug.match(/[a-zA-Z]+/)[0]; // Получаем название никаи из строки
-  let textnum;
-
-  if (slug.includes(".")) {
-    let match = slug.match(/([a-zA-Z]+)(\d+)\.(\d+)/);
-    if (match) {
-      let [, nikaya, subdivision, textnum] = match;
-      let textUrl = findTextUrl(nikaya, parseInt(subdivision), parseInt(textnum));
-      if (textUrl) {
-        return textUrl;
-      }
-    }
-  } else {
-    textnum = parseInt(slug.match(/[a-zA-Z](\d+)/)[1]); // Получаем цифры после букв
-    let textUrl = findTextUrl(nikaya, null, textnum);
-    if (textUrl) {
-      return textUrl;
-    }
-  }
-  
-  return "Запись не найдена";
-}
-
-function findTextUrl(nikaya, subdivision, textnum) {
-  if (subdivision !== null) {
-    if (digitalPaliReader[nikaya].available[subdivision]) {
-      let item = digitalPaliReader[nikaya].available[subdivision].find(item => {
-        if (Array.isArray(item)) {
-          if (item.length === 3) {
-            return textnum >= item[0] && textnum <= item[1];
-          } else if (item.length === 2) {
-            return textnum === item[0];
-          }
-        }
-        return false;
-      });
-      
-      if (item) {
-        return digitalPaliReader.constants.rootUrl + item[item.length - 1];
-      }
-    }
-  } else {
-    let item = digitalPaliReader[nikaya].available.find(item => Array.isArray(item) ? item[0] === textnum : item === textnum);
-    if (item) {
-      return digitalPaliReader.constants.rootUrl + item[1];
-    }
-  }
-  
-  return null;
-}
-
-let textUrl = getTextUrl(slug);
-// console.log("Ссылка на", slug + ":", textUrl);
-if (textUrl) {
-scLink += `<a target="" title="Digital Pali Reader" href="${textUrl}">DPR</a>&nbsp;`;
-}
-}
 //dpr end
 
 scLink += `<a target="" title="SuttaCentral.net" href="https://suttacentral.net/${slug}/en/${translator}">SC</a>&nbsp;`;
