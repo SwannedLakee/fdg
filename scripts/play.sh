f961063a7ea947d5a68909bca9ac72bcefab54f0
o-tab <o-tab@dg.dg>
2026-02-03 08:41:03 +0700

    updates

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,13 +1,38 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo enter X
-read x
+
+# ===== ОБЩИЕ ПЕРЕМЕННЫЕ =====
+
+BASE_URL="https://f.dhamma.gift/memorize/?rp=true&q="
+BASE_AUDIO="/storage/emulated/0/Download/Sutta Audio"
+
+# ===== НАБОР (ВРУЧНУЮ) =====
+
+SET="Mn9#14.4"
+SET="DN22#13.0.2"
+
+AUDIO_FILE="mn91.mp3"
+AUDIO_FILE="dn22-4-1.mp3"
+# AUDIO_FILE="dn22-4-2.mp3"
+
+# ===== НОРМАЛИЗАЦИЯ =====
+
+set_lc="${SET,,}"
+
+# ===== СБОРКА =====
+
+URL="${BASE_URL}${set_lc}"
+AUDIO="${BASE_AUDIO}/${AUDIO_FILE}"
+
+# ===== ЗАПУСК =====
+
+read -p "Интервал в секундах: " x
+
 n=1
-termux-open-url "https://f.dhamma.gift/memorize/?rp=true&q=Mn91#14.4"
-while true; 
-do 
-echo $n
-#play-audio "/storage/emulated/0/Download/Sutta Audio/mn91.mp3";  
-termux-open "/storage/emulated/0/Download/Sutta Audio/mn91.mp3";   
-((n++))
-sleep $x 
+termux-open-url "$URL"
+
+while true; do
+  echo "$n"
+  termux-open "$AUDIO"
+  ((n++))
+  sleep "$x"
 done
