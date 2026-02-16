07dba9fc394acd31f7ad87ecaa511bef3f8131ac
o-tab <o-tab@dg.dg>
2026-02-16 13:01:53 +0700

    updates

--- bipm.php
+++ bipm.php
@@ -52,7 +52,7 @@ include './scripts/opentexts.php';
                     <input type="checkbox" class="form-check-input" id="darkSwitch">
                 </div>
        <a class="ms-1 btn-sm btn-secondary rounded-pill text-decoration-none " href="/pm.php?expand=true" >bu-pm</a>
-      <a class="ms-1 btn-sm btn-secondary rounded-pill text-decoration-none " href="/d/?q=bi-pm&script=devanagari" >bi-pm</a>
+      <a class="ms-1 btn-sm btn-secondary rounded-pill text-decoration-none " href="/?q=bi-pm" >bi-pm</a>
  
     <a class="ms-1 btn-sm btn-secondary rounded-pill text-decoration-none " href="/san/sarv.php" >sarv</a>
     <a class="ms-1 btn-sm btn-secondary rounded-pill text-decoration-none " href="/san/mg.php" >mg</a>
